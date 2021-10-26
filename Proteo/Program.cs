226b2b06d56ad1ab71b4df5947707c4c83c6f41f
Web-Geeks <91744662+Web-Geeks@users.noreply.github.com>
2021-10-26 15:57:54 +0100

    Add project files.

---
+++ Proteo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Proteo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = "Please enter multiple integers (1, 2, 3): ";
+            Console.Write(prompt);
+
+            while (true)
+            {
+                //check to make sure valid values are entered
+                int[] inputValues;
+                while (!TryParseIntegerList(Console.ReadLine(), out inputValues))
+                {
+                    Console.WriteLine("Invalid Input. Try Again...");
+                    Console.Write(prompt);
+                }
+
+                Console.WriteLine(divisible_by_five(inputValues));
+                Console.Write(prompt);
+            }
+        }
+
+        public static bool TryParseIntegerList(string input, out int[] inputValues)
+        {
+            inputValues = default;
+
+            var splits = input.Split(",");
+            var result = new int[splits.Length];
+            if (splits.Where((t, i) => !int.TryParse(t.Trim(), out result[i])).Any())
+            {
+                return false;
+            }
+            inputValues = result;
+
+            return true;
+        }
+
+        public static int divisible_by_five(int[] my_list)
+        {
+            //sum the list to check if the total is less than 100
+
+            var sumOfList = my_list.Sum();
+
+            switch (sumOfList)
+            {
+                case < 100:
+                {
+                    int nInRange = 0;
+                    foreach (int a in my_list)
+                    {
+                        switch (a % 5)
+                        {
+                            case 0:
+                                nInRange++;
+                                break;
+                        }
+                    }
+                    return nInRange;
+                }
+                default:
+                    return 0;
+            }
+        }
+
+       
+
+    }
+}
---
+++ RomanNumerals/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace RomanNumerals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string prompt = "Please enter number (example 5): ";
+            Console.Write(prompt);
+
+            while (true)
+            {
+                //check to make sure valid values are entered
+                int inputValue;
+                while (!TryParseInteger(Console.ReadLine(), out inputValue))
+                {
+                    Console.WriteLine("Invalid Input. Try Again...");
+                    Console.Write(prompt);
+                }
+
+                Console.WriteLine(ConvertToRoman(inputValue));
+                Console.Write(prompt);
+            }
+        }
+
+
+        public static string ConvertToRoman(int n)
+        {
+            if (n < 0)
+                return "Please enter a positive integer";
+            else if (n > 3999)
+            {
+                return "Maximum Number is 3999.  Please try a number between 0 and 3999";
+            }
+            else if (n == 0)
+            {
+                return "Nullus";
+            }
+
+            else
+            {
+                string[] m = { "", "M", "MM", "MMM" };
+                string[] c = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC","DCCC", "CM" };
+                string[] x = { "", "X", "XX", "XXX","XL","L", "LX", "LXX", "LXXX","XC" };
+                string[] i = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+                var thousands = m[n/ 1000];
+                var hundreds = c[(n / 100) % 10];
+                var tens = x[(n / 10) % 10];
+                var ones = i[n % 10];
+
+                var ans = thousands + hundreds + tens + ones;
+
+                return ans;
+            }
+        }
+
+        public static bool TryParseInteger(string input, out int inputValue)
+        {
+            inputValue = default;
+
+            if (int.TryParse(input.Trim(), out inputValue))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
